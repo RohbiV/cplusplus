d637e3c8bd47a9ca03ec5e64e2edb074d5d286fa
Vibhor Kumar Vatsa <vkumarvatsa@gmail.com>
2024-07-03 00:49:09 +0530

    Code for finding prime number and its exe file

---
+++ Revision/prime.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int);
+
+
+int main() {
+    int num;
+    cin >> num;
+    bool res = isPrime(num);
+
+    if(res) cout << "yes" << endl;
+    else cout << "no" << endl;
+
+    return 0;
+}
+
+bool isPrime(int num) {
+    for(int i = 2; i <= num/2; i++) {
+        if(num % i == 0) return false;
+    }
+
+    return true;
+}
